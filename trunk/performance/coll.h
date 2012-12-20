f607d598aca573f903b0d4013aa80a8d0e9e709c
jeff.science@gmail.com <jeff.science@gmail.com@8456ae1e-bbbd-ae23-631b-9bc6519d8e99>
2012-12-20 20:52:42 +0000

    remove consolidated headers

--- trunk/performance/coll.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLL_H
-#define COLL_H
-
-#include <pami.h>
-
-int barrier(pami_geometry_t geometry, pami_context_t context);
-int allgather(pami_geometry_t geometry, pami_context_t context, size_t count, void * sbuf, void * rbuf);
-
-#endif
